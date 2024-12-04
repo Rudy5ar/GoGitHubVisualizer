1a45a320586b4b1248097fcd209552535e88be7a
Petar <petar.rudovic2@gmail.com>
2024-12-04 01:59:04 +0100

    Folder scan

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func scan(folder string) {
@@ -14,20 +17,56 @@ func scan(folder string) {
 
 }
 
+func recursiveScanFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0), folder)
+}
+
+func scanGitFolders(folders []string, folder string) []string {
+	folder = strings.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			println(path)
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
+			}
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
+		}
+	}
+
+	return folders
+}
+
+func addNewSliceElementsToFile(filepath, newRepos []string) {
+	panic("unimplemented")
+}
+
+func getDotFilePath() []string {
+	panic("unimplemented")
+}
+
 func stats(email string) {
 	println("Stats")
-}
-
-func recursiveScanFolder(folder string) {
-	return
-}
-
-func addNewSliceElementsToFile(filepath, repositories) {
-	panic("unimplemented")
-}
-
-func getDotFilePath() {
-	panic("unimplemented")
 }
 
 func main() {
