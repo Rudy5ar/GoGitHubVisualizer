d3084c7697161cd15f86b70ed70acbc3567eb44e
Petar Rudovic <petar.rudovic2@gmail.com>
2024-12-07 12:07:11 +0100

    File scanning

--- main.go
+++ main.go
@@ -1,72 +1,9 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"strings"
-)
-
-func scan(folder string) {
-	println("Found folders: \n")
-	repositories := recursiveScanFolder(folder)
-	filepath := getDotFilePath()
-	addNewSliceElementsToFile(filepath, repositories)
-	fmt.Printf("\n\nSuccessfully added\n\n")
-
-}
-
-func recursiveScanFolder(folder string) []string {
-	return scanGitFolders(make([]string, 0), folder)
-}
-
-func scanGitFolders(folders []string, folder string) []string {
-	folder = strings.TrimSuffix(folder, "/")
-
-	f, err := os.Open(folder)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	files, err := f.Readdir(-1)
-	f.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var path string
-
-	for _, file := range files {
-		if file.IsDir() {
-			path = folder + "/" + file.Name()
-			println(path)
-			if file.Name() == ".git" {
-				path = strings.TrimSuffix(path, "/.git")
-				fmt.Println(path)
-				folders = append(folders, path)
-				continue
-			}
-			if file.Name() == "vendor" || file.Name() == "node_modules" {
-				continue
-			}
-			folders = scanGitFolders(folders, path)
-		}
-	}
-
-	return folders
-}
-
-func addNewSliceElementsToFile(filepath, newRepos []string) {
-	panic("unimplemented")
-}
-
-func getDotFilePath() []string {
-	panic("unimplemented")
-}
+import "flag"
 
 func stats(email string) {
-	println("Stats")
+	println("Stats: ", email)
 }
 
 func main() {
@@ -82,5 +19,4 @@ func main() {
 	}
 
 	stats(email)
-
 }
---
+++ scan.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/user"
+	"strings"
+)
+
+func scan(folder string) {
+	println("Found folders: \n")
+	repositories := recursiveScanFolder(folder)
+	filepath := getDotFilePath()
+	addNewSliceElementsToFile(filepath, repositories)
+	fmt.Printf("\n\nSuccessfully added\n\n")
+
+}
+
+func recursiveScanFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0), folder)
+}
+
+func scanGitFolders(folders []string, folder string) []string {
+	folder = strings.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			println(path)
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
+			}
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
+		}
+	}
+
+	return folders
+}
+
+func getDotFilePath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dotFile := usr.HomeDir + "\\.gogitlocalstats"
+
+	return dotFile
+}
+
+func addNewSliceElementsToFile(filepath string, newRepos []string) {
+	existingRepos := parseFileLinesToSlice(filepath)
+	repos := joinSlices(newRepos, existingRepos)
+	dumpStringsSliceToFile(repos, filepath)
+}
+
+func dumpStringsSliceToFile(repos []string, filepath string) {
+	content := strings.Join(repos, "\n")
+	os.WriteFile(filepath, []byte(content), 0755)
+}
+
+func joinSlices(newRepos []string, existingRepos []string) []string {
+	for _, repo := range newRepos {
+		if !sliceContains(existingRepos, repo) {
+			existingRepos = append(existingRepos, repo)
+		}
+	}
+	return existingRepos
+}
+
+func sliceContains(slice []string, item string) bool {
+	for _, i := range slice {
+		if i == item {
+			return true
+		}
+	}
+	return false
+}
+
+func parseFileLinesToSlice(filepath string) []string {
+	file := openFile(filepath)
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			panic(err)
+		}
+	}
+
+	return lines
+}
+
+func openFile(filepath string) *os.File {
+	file, err := os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		if os.IsNotExist(err) {
+			file, err = os.Create(filepath)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	}
+	return file
+}
