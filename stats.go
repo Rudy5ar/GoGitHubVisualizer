8e732b08e6d536be80e937e75ddd930a8847484e
Petar <petar.rudovic2@gmail.com>
2024-12-12 21:37:26 +0100

    Counting commits per day

--- stats.go
+++ stats.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+)
+
 const outOfRange = 99999
 const daysInLastSixMonths = 183
 const weeksInLastSixMonths = 26
@@ -30,5 +35,35 @@ func processRepositories(email string) map[int]int {
 }
 
 func fillCommits(email string, path string, commits map[int]int) map[int]int {
-	return nil
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		panic(err)
+	}
+
+	head, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+
+	iterator, err := repo.Log(&git.LogOptions{From: head.Hash()})
+	if err != nil {
+		panic(err)
+	}
+
+	err = iterator.ForEach(func(c *object.Commit) error {
+		daysAgo := c.Author.When.Day()
+
+		if c.Author.Email != email {
+			return nil
+		}
+
+		commits[daysAgo]++
+
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return commits
 }
